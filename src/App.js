4bbd3ac417f189974e92daa4aade36fee17bb4f4
sanaz niamati <93905446+sanazniamati@users.noreply.github.com>
2023-01-01 09:48:59 +0330

    3

--- src/App.js
+++ src/App.js
@@ -5,61 +5,81 @@ import { FormCheck } from "react-bootstrap";
 import SwitchExample from "./SwitchExample";
 
 function App() {
-  // 1 pixel (X)	0.0104166667 in
-  const inch = 0.01;
-  // 1 pixel (X)	0.0264583333 in
-  const centimetre = 0.026;
-  const [width, setWidth] = useState(5);
-  const [height, setHeight] = useState(5);
+  // 1cm --> 37.8 pixel
+  // let factor = 37.8;
+  // 1 inch --> 96 pixel
+  // let factor = 96;
+  const [type, setType] = useState("pixel");
+  const [width, setWidth] = useState(20);
+  const [height, setHeight] = useState(20);
+
+  if (type === "centimeter") {
+    let factor = 37.8;
+  } else if (type === "millimeter") {
+    let factor = 3.78;
+  } else if (type === "inch") {
+    let factor = 96;
+  } else {
+    let factor = 1;
+  }
+
   const a = 0;
   const b = 0;
-  const [c, setC] = useState(5);
-  const [d, setD] = useState(10);
+  const [c, setC] = useState(width);
+  const [d, setD] = useState(height);
   const handelOnChangeWidth = (e) => {
     setWidth(e.target.value);
     console.log("width value is:", e.target.value);
-    setC(e.target.value - a);
+    setC((e.target.value - a) * factor);
     console.log("c value is:", e.target.value - a);
   };
   const handelOnChangeHeight = (e) => {
     setHeight(e.target.value);
     console.log("height value is:", e.target.value);
-    setD(e.target.value - a);
+    setD((e.target.value - a) * type);
     console.log("D value is:", e.target.value - d);
   };
-  const handelOnChangeSwitch = () => {};
+  const handelSelect = (e) => {
+    setType(e.target.value);
+    console.log("type value is:", e.target.value);
+  };
   return (
     <>
-      <SwitchExample />
-      {/*<hr />*/}
-      {/*<label> width : </label>*/}
-      {/*<input type={"number"} onChange={handelOnChangeWidth} value={width} />*/}
-      {/*<label> height : </label>*/}
-      {/*<input type={"number"} onChange={handelOnChangeHeight} value={height} />*/}
-      {/*<FormCheck type={"switch"} onChange={handelOnChangeSwitch} />*/}
-      {/*<Stage width={window.innerWidth} height={window.innerHeight}>*/}
-      {/*  <Layer>*/}
-      {/*    <Text text={"a=" + 0 + "  b=" + 0} x={10} y={50} fontSize={20} />*/}
-      {/*    <Text text={"c=width-a  c=" + c} x={10} y={80} fontSize={20} />*/}
-      {/*    <Text text={"d=height-b  d=" + d} x={10} y={100} fontSize={20} />*/}
+      {/*<SwitchExample />*/}
+      <label>choose an unit </label>
+      <select value={type} onChange={handelSelect}>
+        <option value="centimeter">centimeter</option>
+        <option value="millimeter">millimeter</option>
+        <option value="pixel">pixel</option>
+        <option value="inch">inch</option>
+      </select>
+      <label> width : </label>
+      <input type={"number"} onChange={handelOnChangeWidth} value={width} />
+      <label> height : </label>
+      <input type={"number"} onChange={handelOnChangeHeight} value={height} />
+      <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Layer>
+          <Text text={"a=" + 0 + "  b=" + 0} x={10} y={50} fontSize={20} />
+          <Text text={"c=width-a  c=" + c} x={10} y={80} fontSize={20} />
+          <Text text={"d=height-b  d=" + d} x={10} y={100} fontSize={20} />
 
-      {/*    <Shape*/}
-      {/*      x={100}*/}
-      {/*      y={200}*/}
-      {/*      sceneFunc={(context, shape) => {*/}
-      {/*        context.beginPath();*/}
-      {/*        context.moveTo(a, b);*/}
-      {/*        context.lineTo(c, b);*/}
-      {/*        context.lineTo(c, d);*/}
-      {/*        context.lineTo(a, d);*/}
-      {/*        context.closePath();*/}
-      {/*        context.fillStrokeShape(shape);*/}
-      {/*      }}*/}
-      {/*      stroke="red"*/}
-      {/*      strokeWidth={4}*/}
-      {/*    />*/}
-      {/*  </Layer>*/}
-      {/*</Stage>*/}
+          <Shape
+            x={100}
+            y={200}
+            sceneFunc={(context, shape) => {
+              context.beginPath();
+              context.moveTo(a, b);
+              context.lineTo(c, b);
+              context.lineTo(c, d);
+              context.lineTo(a, d);
+              context.closePath();
+              context.fillStrokeShape(shape);
+            }}
+            stroke="red"
+            strokeWidth={4}
+          />
+        </Layer>
+      </Stage>
     </>
   );
 }
--- src/SwitchExample.js
+++ src/SwitchExample.js
@@ -4,28 +4,33 @@ import { useState } from "react";
 import React from "react";
 
 function SwitchExample() {
+  // 1 mm = 3.7795275591 pixel (X)
+  let factor = 37.8;
   const [cmActive, setCmActive] = useState(false);
   const [mmActive, setMmActive] = useState(false);
   const [inActive, setInActive] = useState(false);
-  const [width, setWidth] = useState(5);
-  const [height, setHeight] = useState(5);
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
   const handelOnChangeWidth = (e) => {
     setWidth(e.target.value);
+    console.log("width" + width);
   };
   const handelOnChangeHeight = (e) => {
     setHeight(e.target.value);
+    console.log("height" + height);
   };
-  const handelOnCm = () => {
+
+  const handelCentimeter = () => {
     setCmActive(true);
     setWidth(width / 0.0264583333);
     setHeight(height / 0.0264583333);
   };
-  const handelOnMm = () => {
+  const handelMillimeter = () => {
     setMmActive(true);
     setWidth(width / 0.264583333);
     setHeight(height / 0.264583333);
   };
-  const handelOnIn = () => {
+  const handelInch = () => {
     setInActive(true);
     setWidth(width / 0.0104166667);
     setHeight(height / 0.0104166667);
@@ -33,40 +38,39 @@ function SwitchExample() {
 
   return (
     <>
-      <Form>
-        <Form.Check
-          type="switch"
-          label="centimeter"
-          checked={cmActive}
-          id="centimeter-switch"
-          onClick={handelOnCm}
-        />
-        <Form.Check
-          type="switch"
-          checked={mmActive}
-          id="millimeter-switch"
-          label="millimeter"
-          onClick={handelOnMm}
-        />
-        <Form.Check
-          type="switch"
-          checked={inActive}
-          id="millimeter-switch"
-          label="inch"
-          onClick={handelOnIn}
-        />
-        <label> width : </label>
-        <input type={"number"} onChange={handelOnChangeWidth} value={width} />
-        <label> height : </label>
-        <input type={"number"} onChange={handelOnChangeHeight} value={height} />
-      </Form>
+      <Form.Check
+        type="switch"
+        label="centimeter"
+        checked={cmActive}
+        id="centimeter-switch"
+        // onClick={handelCentimeter}
+      />
+      <Form.Check
+        type="switch"
+        checked={mmActive}
+        id="millimeter-switch"
+        label="millimeter"
+        // onClick={handelMillimeter}
+      />
+      <Form.Check
+        type="switch"
+        checked={inActive}
+        id="millimeter-switch"
+        label="inch"
+        // onClick={handelInch}
+      />
+      <label> width in mm: </label>
+      <input type={"number"} onChange={handelOnChangeWidth} value={width} />
+      <label> height in mm : </label>
+      <input type={"number"} onChange={handelOnChangeHeight} value={height} />
+
       <Stage width={window.innerWidth} height={window.innerHeight}>
         <Layer>
           <Rect
             x={20}
             y={100}
-            width={width}
-            height={height}
+            width={width * factor}
+            height={height * factor}
             fill="red"
             shadowBlur={5}
           />
