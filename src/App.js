59f5802d5e4e7c98a7ca2acc6fc88dafca78caf9
sanaz niamati <93905446+sanazniamati@users.noreply.github.com>
2023-01-02 10:11:45 +0330

    4

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
-import React, { useState } from "react";
-import Form from "react-bootstrap/Form";
-import { Layer, Rect, Shape, Stage, Text } from "react-konva";
-import { FormCheck } from "react-bootstrap";
-import SwitchExample from "./SwitchExample";
+import React, { useEffect, useState } from "react";
+import { Layer, Shape, Stage, Text } from "react-konva";
 
 function App() {
   // 1cm --> 37.8 pixel
-  // let factor = 37.8;
+  // 1mm --> 3.78 pixel
   // 1 inch --> 96 pixel
-  // let factor = 96;
+
   const [type, setType] = useState("pixel");
   const [width, setWidth] = useState(20);
   const [height, setHeight] = useState(20);
 
   if (type === "centimeter") {
-    let factor = 37.8;
+    var factor = 37.8;
   } else if (type === "millimeter") {
-    let factor = 3.78;
+    var factor = 3.78;
   } else if (type === "inch") {
-    let factor = 96;
-  } else {
-    let factor = 1;
+    var factor = 96;
+  } else if (type === "pixel") {
+    var factor = 1;
   }
+  useEffect(() => {
+    // setWidth(width * factor);
+    setC(width * factor - a);
+    // setHeight(height * factor);
+    setD(height * factor - b);
+  }, [type]);
 
   const a = 0;
   const b = 0;
@@ -31,21 +34,21 @@ function App() {
     setWidth(e.target.value);
     console.log("width value is:", e.target.value);
     setC((e.target.value - a) * factor);
-    console.log("c value is:", e.target.value - a);
+    console.log("c value is:", (e.target.value - a) * factor);
   };
   const handelOnChangeHeight = (e) => {
     setHeight(e.target.value);
     console.log("height value is:", e.target.value);
-    setD((e.target.value - a) * type);
-    console.log("D value is:", e.target.value - d);
+    setD((e.target.value - b) * factor);
+    console.log("D value is:", (e.target.value - b) * factor);
   };
   const handelSelect = (e) => {
+    console.log("factor value is:", factor);
     setType(e.target.value);
     console.log("type value is:", e.target.value);
   };
   return (
     <>
-      {/*<SwitchExample />*/}
       <label>choose an unit </label>
       <select value={type} onChange={handelSelect}>
         <option value="centimeter">centimeter</option>
@@ -62,7 +65,6 @@ function App() {
           <Text text={"a=" + 0 + "  b=" + 0} x={10} y={50} fontSize={20} />
           <Text text={"c=width-a  c=" + c} x={10} y={80} fontSize={20} />
           <Text text={"d=height-b  d=" + d} x={10} y={100} fontSize={20} />
-
           <Shape
             x={100}
             y={200}
