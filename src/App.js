6f81a59a3bea1c6416292140f32a1cf7d2befb3c
sanaz niamati <93905446+sanazniamati@users.noreply.github.com>
2023-01-09 18:12:49 +0330

    4

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Layer, Shape, Stage, Text } from "react-konva";
 
 function App() {
@@ -6,46 +6,63 @@ function App() {
   // 1mm --> 3.78 pixel
   // 1 inch --> 96 pixel
 
+  let factor;
+  const stageRef = useRef();
+
   const [type, setType] = useState("pixel");
   const [width, setWidth] = useState(20);
   const [height, setHeight] = useState(20);
 
   if (type === "centimeter") {
-    var factor = 37.8;
+    factor = 37.8;
   } else if (type === "millimeter") {
-    var factor = 3.78;
+    factor = 3.78;
   } else if (type === "inch") {
-    var factor = 96;
+    factor = 96;
   } else if (type === "pixel") {
-    var factor = 1;
+    factor = 1;
   }
   useEffect(() => {
     // setWidth(width * factor);
     setC(width * factor - a);
     // setHeight(height * factor);
     setD(height * factor - b);
-  }, [type]);
+  }, [factor, type, height, width]);
 
   const a = 0;
   const b = 0;
   const [c, setC] = useState(width);
   const [d, setD] = useState(height);
+
   const handelOnChangeWidth = (e) => {
     setWidth(e.target.value);
-    console.log("width value is:", e.target.value);
-    setC((e.target.value - a) * factor);
-    console.log("c value is:", (e.target.value - a) * factor);
+    // console.log("width value is:", e.target.value);
+    // setC((e.target.value - a) * factor);
+    // console.log("c value is:", (e.target.value - a) * factor);
   };
   const handelOnChangeHeight = (e) => {
     setHeight(e.target.value);
-    console.log("height value is:", e.target.value);
-    setD((e.target.value - b) * factor);
-    console.log("D value is:", (e.target.value - b) * factor);
+    // console.log("height value is:", e.target.value);
+    // setD((e.target.value - b) * factor);
+    // console.log("D value is:", (e.target.value - b) * factor);
   };
   const handelSelect = (e) => {
     console.log("factor value is:", factor);
     setType(e.target.value);
     console.log("type value is:", e.target.value);
+  };
+
+  const handelIncWidth = () => {
+    setWidth((prvWidth) => prvWidth + 10);
+  };
+  const handelDecWidth = () => {
+    setWidth((prvWidth) => prvWidth - 10);
+  };
+  const handelIncHeight = () => {
+    setHeight((prvHeight) => prvHeight + 10);
+  };
+  const handelDecHeight = () => {
+    setHeight((prvHeight) => prvHeight - 10);
   };
   return (
     <>
@@ -56,18 +73,28 @@ function App() {
         <option value="pixel">pixel</option>
         <option value="inch">inch</option>
       </select>
-      <label> width : </label>
+      <button onClick={handelDecWidth}> width -</button>
       <input type={"number"} onChange={handelOnChangeWidth} value={width} />
-      <label> height : </label>
+      <button onClick={handelIncWidth}>width +</button>
+
+      <button onClick={handelDecHeight}> height -</button>
       <input type={"number"} onChange={handelOnChangeHeight} value={height} />
-      <Stage width={window.innerWidth} height={window.innerHeight}>
+      <button onClick={handelIncHeight}> height +</button>
+
+      <Stage
+        ref={stageRef}
+        width={window.innerWidth}
+        height={window.innerHeight}
+        style={{ background: "lightpink" }}
+        draggable
+      >
         <Layer>
           <Text text={"a=" + 0 + "  b=" + 0} x={10} y={50} fontSize={20} />
           <Text text={"c=width-a  c=" + c} x={10} y={80} fontSize={20} />
           <Text text={"d=height-b  d=" + d} x={10} y={100} fontSize={20} />
           <Shape
-            x={100}
-            y={200}
+            x={10}
+            y={10}
             sceneFunc={(context, shape) => {
               context.beginPath();
               context.moveTo(a, b);
@@ -77,7 +104,8 @@ function App() {
               context.closePath();
               context.fillStrokeShape(shape);
             }}
-            stroke="red"
+            stroke="blue"
+            draggable
             strokeWidth={4}
           />
         </Layer>
