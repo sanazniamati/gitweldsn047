736cf3fcfa0b329e885337321622045b65c429ab
sanaz niamati <93905446+sanazniamati@users.noreply.github.com>
2023-01-19 10:03:14 +0330

    commit-3 in TDD branch  add test1

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,9 @@
 import App from "./App";
 import { fireEvent, render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
+test("Increase width render with + ", () => {
+  const { getByTestId } = render(<App />);
+  // eslint-disable-next-line testing-library/prefer-screen-queries
+  const widthInc = getByTestId("width-inc");
+  expect(widthInc.textContent).toBe("width +");
+});
