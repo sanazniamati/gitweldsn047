983a96f9d0cc254bb5615c443b2710fd4b12951c
sanaz niamati <93905446+sanazniamati@users.noreply.github.com>
2023-01-19 09:57:09 +0330

    commit rebase master onto TDD

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,13 @@
 import App from "./App";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+test("Increase width render with + ", () => {
+  const { getByTestId } = render(<App />);
+  // eslint-disable-next-line testing-library/prefer-screen-queries
+  const widthInc = getByTestId("width-inc");
+  expect(widthInc.textContent).toBe("width +");
+});
+import App from "./App";
 import { fireEvent, render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 test("Increase width render with + ", () => {
