5eb9dd05466c3f8eab72a953184f937ed4c1f2f9
sanaz niamati <93905446+sanazniamati@users.noreply.github.com>
2023-01-19 10:04:13 +0330

    commit-4 in TDD branch  add test2

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,15 @@ test("Increase width render with + ", () => {
   const widthInc = getByTestId("width-inc");
   expect(widthInc.textContent).toBe("width +");
 });
+test("change value of input width works correctly", () => {
+  const { getByTestId } = render(<App />);
+  // eslint-disable-next-line testing-library/prefer-screen-queries
+  const inputWidth = getByTestId("width-input");
+  expect(inputWidth.value).toBe("20");
+  fireEvent.change(inputWidth, {
+    target: {
+      value: "35",
+    },
+  });
+  expect(inputWidth.value).toBe("35");
+});
