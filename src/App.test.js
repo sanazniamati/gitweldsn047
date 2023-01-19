4773b0b3a350892d04d361e080d27b667ae77d0f
sanaz niamati <93905446+sanazniamati@users.noreply.github.com>
2023-01-19 10:05:03 +0330

    commit-5 in TDD branch  add tests

--- src/App.test.js
+++ src/App.test.js
@@ -19,3 +19,32 @@ test("change value of input width works correctly", () => {
   });
   expect(inputWidth.value).toBe("35");
 });
+test("change value of input height works correctly", () => {
+  const { getByTestId } = render(<App />);
+  // eslint-disable-next-line testing-library/prefer-screen-queries
+  const inputHeight = getByTestId("height-input");
+  expect(inputHeight.value).toBe("20");
+  fireEvent.change(inputHeight, {
+    target: {
+      value: "35",
+    },
+  });
+  expect(inputHeight.value).toBe("35");
+});
+test("changeing value of input height with onClick height+ works correctly", () => {
+  const { getByTestId } = render(<App />);
+  // eslint-disable-next-line testing-library/prefer-screen-queries
+  const inputHeight = getByTestId("height-input");
+  // eslint-disable-next-line testing-library/prefer-screen-queries
+  const heightInc = getByTestId("height-inc");
+  expect(inputHeight.value).toBe("20");
+  fireEvent.change(inputHeight, {
+    target: {
+      value: "35",
+    },
+  });
+  expect(inputHeight.value).toBe("35");
+  fireEvent.click(heightInc);
+  fireEvent.click(heightInc);
+  expect(inputHeight.value).toBe("55");
+});
